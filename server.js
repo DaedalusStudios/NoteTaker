1ac7fab50f25b6dd982b1d5521f2b3b360919c9a
= <the.digital.avatar@gmail.com>
2023-12-05 22:20:50 -0700

    heroku port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const fsp = require('fs').promises;
 const uniqid = require('uniqid');
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
